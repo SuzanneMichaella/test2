6b088edd9b4cb7e8afa8a28db839b302e4f45f7b
SuzanneMichaella <92297087+SuzanneMichaella@users.noreply.github.com>
2021-11-19 02:59:51 +0100

    Update main.ts, main.py, images.g.jres, images.g.ts

--- main.py
+++ main.py
@@ -5,22 +5,56 @@ duck = my_sprite
 scene.camera_follow_sprite(duck)
 controller.move_sprite(duck)
 
-my_sprite2 = sprites.create(assets.image("""za"""), SpriteKind.player)
-za=my_sprite2
+#items
+za = sprites.create(assets.image("""za"""), SpriteKind.projectile)
+za.x=100
+za.y=20
 
-my_sprite3 = sprites.create(assets.image("""stump"""), SpriteKind.player)
-stump= my_sprite3
+apple = sprites.create(assets.image("""apple"""), SpriteKind.projectile)
+apple.x = 50 
+apple.y=20
+
+cake= sprites.create(assets.image('''cake'''), SpriteKind.projectile)
+cake.x=150
+cake.y=20
+
+#bins
+stump = sprites.create(assets.image("""stump"""), SpriteKind.enemy)
+stump.y= 100
+
+tree= sprites.create(assets.image("""tree"""), SpriteKind.enemy)
+tree.y=stump.y
+tree.x = stump.x-50
 
 
-def on_button_event_a_pressed():
-    if duck.overlaps_with(za):
-        za.follow(duck, 75)
-controller.player1.on_button_event(ControllerButton.A, ControllerButtonEvent.PRESSED, on_button_event_a_pressed)
+#where they go
+slist =[stump, za, cake]
+tlist = [tree, apple, cake]
 
-def on_button_event_b_pressed():
-    za.follow(None)
-    if duck.overlaps_with(stump):
-        info.set_score(1)
-        za.destroy()
-controller.B.on_event(ControllerButtonEvent.PRESSED, on_button_event_b_pressed)
+#picking up and dropping off
+info.set_score(0)
+
+def on_on_overlap(sprite, otherSprite):
+    
+    def on_button_event_a_pressed():#up
+            if duck.overlaps_with(otherSprite):
+                otherSprite.follow(duck, 75)
+    controller.player1.on_button_event(ControllerButton.A, ControllerButtonEvent.PRESSED, on_button_event_a_pressed)
+
+    def on_button_event_b_pressed():#down
+        otherSprite.follow(None)
+        if duck.overlaps_with(stump):
+            if otherSprite in slist:
+                info.change_score_by(1)
+                otherSprite.destroy()
+        if duck.overlaps_with(tree):
+            if otherSprite in tlist:
+                info.change_score_by(2)
+                otherSprite.destroy()
+
+    controller.B.on_event(ControllerButtonEvent.PRESSED, on_button_event_b_pressed)
+    
+sprites.on_overlap(SpriteKind.player, SpriteKind.projectile, on_on_overlap)
+
+
 
--- main.ts
+++ main.ts
@@ -3,21 +3,53 @@ let my_sprite = sprites.create(assets.image`duck`, SpriteKind.Player)
 let duck = my_sprite
 scene.cameraFollowSprite(duck)
 controller.moveSprite(duck)
-let my_sprite2 = sprites.create(assets.image`za`, SpriteKind.Player)
-let za = my_sprite2
-let my_sprite3 = sprites.create(assets.image`stump`, SpriteKind.Player)
-let stump = my_sprite3
-controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function on_button_event_a_pressed() {
-    if (duck.overlapsWith(za)) {
-        za.follow(duck, 75)
-    }
-    
-})
-controller.B.onEvent(ControllerButtonEvent.Pressed, function on_button_event_b_pressed() {
-    za.follow(null)
-    if (duck.overlapsWith(stump)) {
-        info.setScore(1)
-        za.destroy()
-    }
-    
+// items
+let za = sprites.create(assets.image`za`, SpriteKind.Projectile)
+za.x = 100
+za.y = 20
+let apple = sprites.create(assets.image`apple`, SpriteKind.Projectile)
+apple.x = 50
+apple.y = 20
+let cake = sprites.create(assets.image`cake`, SpriteKind.Projectile)
+cake.x = 150
+cake.y = 20
+// bins
+let stump = sprites.create(assets.image`stump`, SpriteKind.Enemy)
+stump.y = 100
+let tree = sprites.create(assets.image`tree`, SpriteKind.Enemy)
+tree.y = stump.y
+tree.x = stump.x - 50
+// where they go
+let slist = [stump, za, cake]
+let tlist = [tree, apple, cake]
+// picking up and dropping off
+info.setScore(0)
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function on_on_overlap(sprite: Sprite, otherSprite: Sprite) {
+    controller.player1.onButtonEvent(ControllerButton.A, ControllerButtonEvent.Pressed, function on_button_event_a_pressed() {
+        // up
+        if (duck.overlapsWith(otherSprite)) {
+            otherSprite.follow(duck, 75)
+        }
+        
+    })
+    controller.B.onEvent(ControllerButtonEvent.Pressed, function on_button_event_b_pressed() {
+        // down
+        otherSprite.follow(null)
+        if (duck.overlapsWith(stump)) {
+            if (slist.indexOf(otherSprite) >= 0) {
+                info.changeScoreBy(1)
+                otherSprite.destroy()
+            }
+            
+        }
+        
+        if (duck.overlapsWith(tree)) {
+            if (tlist.indexOf(otherSprite) >= 0) {
+                info.changeScoreBy(2)
+                otherSprite.destroy()
+            }
+            
+        }
+        
+    })
 })
